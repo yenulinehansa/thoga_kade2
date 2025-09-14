33872f2db7570de868cbb88a453cce65d9650515
yenulinehansa <yenulinehansa739@gmail.com>
2025-09-14 10:31:47 +0530

    Initial comment

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main (String[] args) {
+       Starter.main(args);
+    }
+
+}
